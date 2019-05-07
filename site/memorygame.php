72aa0e4a92c9fc8dc35f54139c7f1c62f769e2c5
Daniel Coady <daniel.coady@outlook.com.au>
2019-05-07 22:10:49 +1000

    added base for memory game front end

--- site/memorygame.php
+++ site/memorygame.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Memory Game</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/bootstrap.css">
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -25,8 +25,11 @@
         </div>
         
         <div class="row">
-            <div class="col-auto">
+            <div class="col">
                 <?php include "highscores.php"; ?>
+            </div>
+            <div class="col">
+                <?php include "playerinfo.php"; ?>
             </div>
         </div>
     </main>
---
+++ site/playerinfo.php
@@ -0,0 +1,12 @@
+<?php
+    // show any errors
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    //TODO: show current player progess and score
+    $level = 0;
+    $levelProgress = 0;
+    echo "<p>Level: " . $level . "</p>";
+    echo "<p>Progress: " . $levelProgress . "</p>";
+?>
